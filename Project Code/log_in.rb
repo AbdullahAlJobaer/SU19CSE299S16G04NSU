f304f60bdd0b3cb6af87a22157c8ce3d1bbf9ee1
Md. Sadman Ahmed <abirsadmanahmed177@gmail.com>
2019-07-07 00:01:55 +0600

    Issue #18 : Updated
    Updated some new fiields for login class

--- Project Code/log_in.rb
+++ Project Code/log_in.rb
@@ -3,9 +3,10 @@ class CreateLog_In < ActiveRecord::Migration[5.2]
     create_table :log_in do |t|
       t.string   :name
       t.decimal  :password
+      t.decimal  :repeat_password
 
 
-      t.string :email
+      t.string :email_address
 
       t.timestamps
     end
