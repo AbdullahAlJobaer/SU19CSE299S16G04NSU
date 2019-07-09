9a9e526a5f133ff9e31463286874d9ad0d2b0025
Md. Sadman Ahmed <abirsadmanahmed177@gmail.com>
2019-07-10 00:38:38 +0600

    Issue #18 Updated the validation
    Changed the validation of password

--- Project Code/loginvalid.rb
+++ Project Code/loginvalid.rb
@@ -2,7 +2,7 @@ class Log_In < ApplicationRecord
 
   validates :name, presence: true , uniqueness: true
 
-  validates :password  confirmation: true, if: :password_required?
+  validates :password  ,presence: true, length: 8..20,confirmation: true, if: :password_required?
 
   validates :email ,presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create } , email: true
 
