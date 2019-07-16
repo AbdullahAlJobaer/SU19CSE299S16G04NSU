fbf6a51f0bfe990bce720b01d50067712f5fcb8c
Md. Sadman Ahmed <abirsadmanahmed177@gmail.com>
2019-07-16 17:23:02 +0600

    Issue #26  Updated the Condition
    Validation condition Has been updated

--- Project Code/paste_valid.rb
+++ Project Code/paste_valid.rb
@@ -1,13 +1,13 @@
 class Paste < ApplicationRecord
 
-  validates :event_name
+  validates :event_name, length:{maximum: 400 } ,presence: true , uniqueness: false
 
-  validates :event_details
+  validates :event_details, length:{maximum: 500 } ,presence: true , uniqueness: false
 
-  validates :date 
-   
-  validates :time 
+  validates :date  , length:{maximum: 40 } ,presence: true , uniqueness: false
 
-  validates :place
+  validates :time , length:{maximum: 40 } ,presence: true , uniqueness: false
+
+  validates :place, length:{maximum: 30 } ,presence: true , uniqueness: false
 
 end
