f6cccf2e448bf0fd2cd9e445447869176d07bc31
Md. Sadman Ahmed <abirsadmanahmed177@gmail.com>
2019-07-21 21:19:18 +0600

    Feature #30 : Update User
    Updated the user data fields with name,password and email thus in user menu they can modify it

--- Project Code/user.rb
+++ Project Code/user.rb
@@ -1,7 +1,11 @@
 class CreateUser< ActiveRecord::Migration[5.2]
   def change
     create_table :user do |t|
+      t.string   :name
+      t.string  :password
       
+
+      t.string :email_address
       
 
       t.timestamps
